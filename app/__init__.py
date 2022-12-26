2d1597d8d4f90888e6d1e9d625aa837aeee06c9a
Hanna Yaremii <anyta070403@gmail.com>
2022-12-26 21:30:14 +0200

    fixed bugs

--- app/__init__.py
+++ app/__init__.py
@@ -33,7 +33,7 @@ def create_app(config_name = 'default'):
     admin.init_app(app, index_view=IndexAdmin())
     from .account.models import User
     from .task.models import Task, Category 
-    admin.add_view(AdminModelView(User, db.session, name='Users', endpoint="users_"))
+    admin.add_view(AdminModelView(User, db.session, name='Users', endpoint="allusers_"))
     admin.add_view(TodoModelView(Task, db.session, name='Tasks', endpoint="tasks_"))
     admin.add_view(AdminModelView(Category, db.session, name='Categories', endpoint="categories_"))
     admin.add_view(CustomFileAdmin(app.static_folder, '/static/', name='Static Files'))
